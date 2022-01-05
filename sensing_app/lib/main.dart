66d34b5b674fb267d391a3a630738fb3dfe766f5
Lucia Montesinos <lucia.montesinos@live.u-tad.com>
2022-01-05 12:07:49 +0100

    Added GPS, Steps & Activity type sensing

--- sensing_app/lib/main.dart
+++ sensing_app/lib/main.dart
@@ -1,115 +1,181 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:location/location.dart' as loc;
+import 'package:pedometer/pedometer.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:activity_recognition_flutter/activity_recognition_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(SensingApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+/// A Flutter application demonstrating the functionality of this plugin
+class SensingApp extends StatelessWidget {
+  /// [MaterialColor] to be used in the app [ThemeData]
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    /// Returns a [List] with [IconData] to show in the [AppHome] [AppBar].
+    final List<IconData> icons = [
+      Icons.location_on,
+      Icons.info_outline,
+    ];
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Sensing app',
+      home: AppHome(title: 'Sensing app'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+/// A Flutter example demonstrating how the [pluginName] plugin could be used
+class AppHome extends StatefulWidget {
+  /// Constructs the [AppHome] class
+  AppHome({required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  /// The [title] of the application, which is shown in the application's
+  /// title bar.
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _AppHomeState createState() => _AppHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AppHomeState extends State<AppHome> {
+  late Stream<StepCount> _stepCountStream;
+  late Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '?';
+  loc.Location location = loc.Location();
+  List<Placemark>? placemarks;
+  loc.LocationData? _locationData;
+  StreamSubscription<ActivityEvent>? activityStreamSubscription;
+  List<ActivityEvent> _events = [];
+  ActivityRecognition activityRecognition = ActivityRecognition.instance;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    checkPermissions();
+    getLocation();
+    initPlatformState();
+    _init();
+    _events.add(ActivityEvent.empty());
+    super.initState();
+  }
+
+  void _init() async {
+    // Android requires explicitly asking permission
+    if (Platform.isAndroid) {
+      if (await Permission.activityRecognition.request().isGranted) {
+        _startTracking();
+      }
+    }
+
+    // iOS does not
+    else {
+      _startTracking();
+    }
+  }
+
+  void _startTracking() {
+    activityStreamSubscription =
+        activityRecognition.activityStream(runForegroundService: true).listen(onData);
+  }
+
+  void checkPermissions() async {
+    bool _serviceEnabled;
+    loc.PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == loc.PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != loc.PermissionStatus.granted) {
+        return;
+      }
+    }
+  }
+
+  void getLocation() async {
+    _locationData = await location.getLocation();
+    placemarks = await placemarkFromCoordinates(_locationData!.latitude!, _locationData!.longitude!);
+    setState(() {});
+
+    //print(_locationData.latitude);
+  }
+
+  void onStepCount(StepCount event) {
+    print("@@@ $event");
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _steps = event.steps.toString();
     });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    print(event);
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    print('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    print(_status);
+  }
+
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  void onData(ActivityEvent activityEvent) {
+    print('ACTIVITY - $activityEvent');
+    setState(() {
+      _events.add(activityEvent);
+    });
+  }
+
+  void initPlatformState() {
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+    _pedestrianStatusStream.listen(onPedestrianStatusChanged).onError(onPedestrianStatusError);
+
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text("The coordinates are :  ${_locationData!.latitude!}, ${_locationData!.longitude!}"),
+        Text("The place is: " + placemarks!.last.toString()),
+        Text("Steps taken: " + _steps),
+        Text(
+            "Current activity :  ${_events.last.type.toString().split('.').last} (${_events.last.confidence}"),
+      ])),
     );
+  }
+
+  @override
+  void dispose() {
+    activityStreamSubscription?.cancel();
+    super.dispose();
   }
 }
