a395eeba57b467ec0c020a2e8370978eccc75871
Lucia Montesinos <lucia.montesinos@live.u-tad.com>
2022-01-12 17:13:06 +0100

    Bluetooth

---
+++ raspberry/bluetooth.py
@@ -0,0 +1,18 @@
+import pexpect
+
+child = pexpect.spawn("bluetoothctl")
+child.send("scan on\n")
+bdaddrs = []
+
+try:
+    while True:
+        child.expect(
+            "Device (([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2}))", timeout=None)
+        bdaddr = str(child.match.group(1))
+        print(bdaddr)
+        if bdaddr not in bdaddrs:
+            bdaddrs.append(bdaddr)
+            if bdaddr == "b'A8:91:3D:61:E9:E4'":
+                print("The phone of lucia is nearby")
+except KeyboardInterrupt:
+    child.close()
