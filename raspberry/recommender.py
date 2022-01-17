526c33deede897a1424a34501156116cf32e3430
Marleen <mk@inniti.dk>
2022-01-17 16:48:46 +0100

    light exposure and activity estimation

--- raspberry/recommender.py
+++ raspberry/recommender.py
@@ -1,3 +1,4 @@
+from pickle import FALSE, TRUE
 import psycopg2
 import random
 import pygame
@@ -100,8 +101,37 @@ def checkLocation(today, location_table):
     pass
 
 
-def checkActivity(today, activity_table):
-    pass
+def checkActivity(today, activities_table):
+    ACTIVITY_DURATION   = 5
+    walking_duration    = 0
+    running_duration    = 0
+    still_duration      = 0
+    biking_duration     = 0
+
+    for row in activities_table:
+        if 'WALKING' == row[1]:
+            walking_duration += ACTIVITY_DURATION
+        elif 'RUNNING' == row[1]:
+            running_duration += ACTIVITY_DURATION
+        elif 'STILL' == row[1]:
+            still_duration += ACTIVITY_DURATION
+        elif 'ON_BICYCLE' == row[1]:
+            biking_duration += ACTIVITY_DURATION
+ 
+    outdoor_activity_duration = walking_duration + running_duration + biking_duration
+
+    return outdoor_activity_duration
+
+""" different activity types:
+IN_VEHICLE
+ON_BICYCLE
+ON_FOOT
+RUNNING
+STILL
+TILTING
+UNKNOWN
+WALKING
+INVALID (used for parsing errors) """
 
 
 def checkSteps(today, steps_table):
@@ -122,16 +152,16 @@ def obtainActivityRecommendation():
             # check tomorrow weather
         else:
             activity_recommendation.append(
-                "You still have time to reach the goal. Here are some recommendation based on the weather forecast")
+                "You still have time to reach the goal. Here are some recommendations based on the weather forecast.")
             # check todays weather
 
     elif current_steps < 10000 and (hasActivityRunning or hasActivityCycling):
         activity_recommendation.append(
-            "Congratulations you reached the recommended actvity goal for the day! Keep it like this")
+            "Congratulations! You reached the recommended actvity goal for the day! Keep it like this.")
 
     elif current_steps >= 10000:
         activity_recommendation.append(
-            "Congratulations you reached the recommended actvity goal for the day! Keep it like this")
+            "Congratulations! You reached the recommended actvity goal for the day! Keep it like this.")
 
     return activity_recommendation
 
@@ -155,19 +185,54 @@ def obtainMusicRecommendation(music_library):
 
     return music_recommendation, path
 
-
 def obtainLightRecommendation():
     vitamin_intake = ""
     enough_light = False
     light_recommendation = ""
+    outdoor_activity_today = False
+    SUFFICIENT_LIGHT = 200
+    SUFFICIENT_OUTDOOR_ACTIVITY = 30
+
+    # light_exposure is calculated by cloudiness during the last day
+    # calculate cloudiness average
+    cloud_sum   = 0 
+    cloud_count = 0
+    cloud_avg   = 0
+    for row in weather_table:
+        cloud_sum += row[2]
+        cloud_count += 1
+    cloud_avg = cloud_sum / cloud_count
+
+    # calculate uvi average
+    uvi_sum     = 0 
+    uvi_count   = 0
+    uvi_avg     = 0
+    for row in weather_table:
+        uvi_sum += row[1]
+        uvi_count += 1
+    uvi_avg = uvi_sum / uvi_count
+
+    # calculate outdoor_activity 
+    outdoor_activity_duration = checkActivity()
+
+    # combine outdoor_activity with cloudiness average to get light_exposure
+    UVI_SCALING = 2
+    light_exposure = (100 - cloud_avg) * uvi_avg * UVI_SCALING
+    # sufficient_exp = (100 - 50) * 2 * UVI_SCALING
+    
+
+    if SUFFICIENT_LIGHT <= light_exposure and SUFFICIENT_OUTDOOR_ACTIVITY <= outdoor_activity_duration:
+        enough_light = TRUE
+    else:
+        enough_light = FALSE
 
     if not enough_light:
         # If not enough light, recommend vitamin intake
         vitamin_intake.append(
-            "You didn't get enough light exposure today. To compensate you could take ?? of vitamin")
+            "You didn't get enough light exposure today. To compensate you could take one drop of vitamin D, which equals 1000 I.U.")
 
         # Turn on the lamp. For how long?
-        minutes_lamp = 10
+        minutes_lamp = 30
 
         light_recommendation.append(
             "Please come closer to the device, the lamp will be turned on for the next ")
@@ -176,7 +241,7 @@ def obtainLightRecommendation():
 
     else:
         vitamin_intake.append(
-            "You don't need to take a supplementary vitamin today")
+            "You got enough sunlight and don't need to take a supplementary vitamin today")
         minutes_lamp = 0
 
     return light_recommendation, vitamin_intake, minutes_lamp
